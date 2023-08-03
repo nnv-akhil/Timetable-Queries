aae0302e86f98813d342ad5587cf1346a0a7c5f7
Akhil Nerella <92852625+nnv-akhil@users.noreply.github.com>
2023-08-03 13:51:49 +0530

    Update app.py

--- Timetable-Queries/app.py
+++ Timetable-Queries/app.py
@@ -1,5 +1,6 @@
-from flask import *
 from pathlib import Path
+
+from flask import *
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
@@ -10,24 +11,24 @@ def home():
 @app.route('/get_dropdown_data', methods=['POST'])
 def get_dropdown_data():
     form_input = request.form.get("q")
-    if form_input == "1":
-        list_data = {'teacher': '10', 'user': '100'}
-    elif form_input == "2":
-        list_data = {'teacher': '20', 'user': '200'}
-    elif form_input == "3":
-        list_data = {'teacher': '30', 'user': '300'}
-    elif form_input == "4":
-        list_data = {'teacher': '40', 'user': '400'}
-    elif form_input == "5":
-        list_data = {'teacher': '50', 'user': '500'}
-    elif form_input == "6":
-        list_data = {'teacher': '60', 'user': '600'}
+    if form_input == "admin-2":
+        list_data = {'q':teachers}
+    elif form_input == "admin-3":
+        list_data = {'q':teachers}
+    elif form_input == "admin-5":
+        list_data = {'q': classes}
+    elif form_input == "admin-6":
+        list_data = {'q': classes}
+    elif form_input == "admin-8":
+        list_data = {'q': places}
+    elif form_input == "admin-9":
+        list_data = {'q': places}
     else:
         list_data = {} 
     print(form_input)
     return jsonify(result=list_data)
 
-@app.route('/form1', methods=['POST'])
+@app.route('/admin-form1', methods=['POST'])
 def form1():
     teacher_name = request.form.get("teacher_name")
     username = request.form.get("username")
@@ -38,14 +39,14 @@ def form1():
     print(csv_file)
     return "Data received successfully"
 
-@app.route('/form2', methods=['POST'])
+@app.route('/admin-form2', methods=['POST'])
 def form2() :
     teacher_name = request.form.get("teacher_name")
 
     print(teacher_name)
     return "Data received successfully"
 
-@app.route('/form3', methods=['POST'])
+@app.route('/admin-form3', methods=['POST'])
 def form3():
     teacher_name = request.form.get("teacher_name")
     csv_data = request.form.get("csv_data")
@@ -54,23 +55,23 @@ def form3():
     print(csv_data)
     return "Data received successfully"
 
-@app.route('/form4', methods=['POST'])
+@app.route('/admin-form4', methods=['POST'])
 def form4():
     class_name = request.form.get("class_name")
     csv_data = request.form.get("csv_data")
 
     print(class_name)
     print(csv_data)
-    return "Data received successfully"
+    return "Data received successfully"x
 
-@app.route('/form5', methods=['POST'])
+@app.route('/admin-form5', methods=['POST'])
 def form5():
     class_name = request.form.get("class_name")
 
     print(class_name)
     return "Data received successfully"
 
-@app.route('/form6', methods=['POST'])
+@app.route('/admin-form6', methods=['POST'])
 def form6():
     class_name = request.form.get("class_name")
     csv_data = request.form.get("csv_data")
@@ -79,7 +80,7 @@ def form6():
     print(csv_data)
     return "Data received successfully"
 
-@app.route('/form7', methods=['POST'])
+@app.route('/admin-form7', methods=['POST'])
 def form7():
     place_name = request.form.get("place_name")
     location_name = request.form.get("location_name")
@@ -88,14 +89,14 @@ def form7():
     print(location_name)
     return "Data received successfully"
 
-@app.route('/form8', methods=['POST'])
+@app.route('/admin-form8', methods=['POST'])
 def form8():
     place_name = request.form.get("place_name")
 
     print(place_name)
     return "Data received successfully"
 
-@app.route('/form9', methods=['POST'])
+@app.route('/admin-form9', methods=['POST'])
 def form9():
     place_name = request.form.get("place_name")
     location_name = request.form.get("location_name")
